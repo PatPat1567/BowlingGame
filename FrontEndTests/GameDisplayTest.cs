a6e640e7c9576c060d9c34289d12ca5aa90ace54
Patrick Towle <towle_patrick@yahoo.com>
2022-09-28 22:47:34 -0500

    Add button tests

--- FrontEndTests/GameDisplayTest.cs
+++ FrontEndTests/GameDisplayTest.cs
@@ -1,3 +1,4 @@
+using System;
 using FrontEnd.Shared;
 using FrontEnd.Data;
 using Bunit;
@@ -16,10 +17,10 @@ public class GameDisplayTest
 
         ctx.Services.AddSingleton<GameService>(new GameService());
 
-        // Render Counter component.
+        // Render GameDisplay component.
         var component = ctx.RenderComponent<GameDisplay>();
 
-        // Assert: first, find the parent_name vital element, then verify its content.
+        // Assert
         Assert.Equal("Game Score: 0", component.Find($".game-score").TextContent.Replace("\n", "").Trim());
         Assert.Equal("Roll Ball", component.Find($".roll").TextContent);
         Assert.Equal("Restart Game", component.Find($".restart").TextContent);
@@ -27,10 +28,63 @@ public class GameDisplayTest
     }
 
     [Fact]
+    public void RollBallButtonClick()
+    {
+        using var ctx = new TestContext();
+        ctx.Services.AddSingleton<GameService>(new GameService());
+
+        var component = ctx.RenderComponent<GameDisplay>();
+
+        var RollValueException = Assert.Throws<Bunit.ElementNotFoundException>(() => component.Find($".roll-value"));
+
+        
+        var RollBallButton = component.Find($".roll");
+        RollBallButton.Click();
+
+        var RollValue = component.Find($".roll-value").TextContent;
+        int.TryParse(RollValue, out int RollValueInt);
+        Assert.InRange(RollValueInt, 0, 10);
+    }
+
+    [Fact]
+    public void SimulateGameButtonClick()
+    {
+        using var ctx = new TestContext();
+        ctx.Services.AddSingleton<GameService>(new GameService());
+
+        var component = ctx.RenderComponent<GameDisplay>();
+
+        var gameScore = "0";
+        Assert.Equal(gameScore, component.Find($".score").TextContent);
+
+        var SimulateGameButton = component.Find($".simulate");
+        SimulateGameButton.Click();
+        gameScore = component.Find($".score").TextContent;
+        int.TryParse(gameScore, out int gameScoreInt);
+        Assert.InRange(gameScoreInt, 0, 300);
+    }
+
+    [Fact]
     public void RestartGameButtonClick()
     {
         using var ctx = new TestContext();
-        Assert.True(true);
+        ctx.Services.AddSingleton<GameService>(new GameService());
+
+        var component = ctx.RenderComponent<GameDisplay>();
+
+        var gameScore = "0";
+        Assert.Equal(gameScore, component.Find($".score").TextContent);
+
+        var SimulateGameButton = component.Find($".simulate");
+        SimulateGameButton.Click();
+        gameScore = component.Find($".score").TextContent;
+        int.TryParse(gameScore, out int gameScoreInt);
+        Assert.InRange(gameScoreInt, 0, 300);
+
+        var ResetGameButton = component.Find($".restart");
+        ResetGameButton.Click();
+        gameScore = "0";
+        Assert.Equal(gameScore, component.Find($".score").TextContent);
     }
 
 }
